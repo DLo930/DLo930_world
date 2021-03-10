28e76146e84ddefe2045d7397ef3e3d8b733e5d1
Alec Oshita <oshita.alec@gmail.com>
2021-03-10 10:36:38 -0500

    Added scopes and validatinos

--- app/models/aritcle.rb
+++ app/models/aritcle.rb
@@ -1,3 +1,10 @@
 class Aritcle < ApplicationRecord
+
   has_one :category
+
+  validates_presence_of :title, :content
+
+  scope :alphabetical, -> { order('title') }
+  scope :active, 			 -> { where('active = ?', true) }
+
 end
