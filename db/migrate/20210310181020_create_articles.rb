bbb2b84725d4a0c6d101a0268c5174129a20ea54
Alec Oshita <oshita.alec@gmail.com>
2021-03-10 13:15:29 -0500

    added articles controller and views

--- db/migrate/20210310181020_create_articles.rb
+++ db/migrate/20210310181020_create_articles.rb
@@ -1,6 +1,6 @@
-class CreateAritcles < ActiveRecord::Migration[5.1]
+class CreateArticles < ActiveRecord::Migration[5.1]
   def change
-    create_table :aritcles do |t|
+    create_table :articles do |t|
       t.string :title
       t.text :content
       t.integer :category_id
